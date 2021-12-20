191c8ebd7aca58e9aabcd8f86e07898b96a7e600
Hasan Erdal <hasaerda@hotmail.com>
2021-12-20 15:07:07 +0300

    Implement Generic Repository Pattern

---
+++ ContactApp.API/Data/Repositories/GenericRepository.cs
@@ -0,0 +1,111 @@
+﻿using ContactApp.API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ContactApp.API.Data.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class, IEntity
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public GenericRepository(ContactDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(object id)
+        {
+            var entity = _dbSet.Find(id);
+            if (entity != null)
+            {
+                _context.Entry(entity).State = EntityState.Detached;
+            }
+            _dbSet.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public TEntity GetById(object id)
+        {
+            var entity = _dbSet.Find(id);
+            if (entity != null)
+            {
+                _context.Entry(entity).State = EntityState.Detached;
+            }
+            return entity;
+        }
+
+        public async Task<TEntity> GetByIdAsync(object id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity != null)
+            {
+                _context.Entry(entity).State = EntityState.Detached;
+            }
+            return entity;
+        }
+
+        public void Insert(TEntity entity)
+        {
+            _dbSet.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public async Task InsertAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<TEntity> InsertAsyncReturn(TEntity entity)
+        {
+            var data = await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return data.Entity;
+        }
+
+        public TEntity InsertReturn(TEntity entity)
+        {
+            var data = _dbSet.Add(entity);
+            _context.SaveChanges();
+            return data.Entity;
+        }
+
+        public IQueryable<TEntity> JoinWhere(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includes)
+        {
+            IQueryable<TEntity> query = _dbSet;
+            query = query.Where(predicate);
+            if (includes.Any())
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+            return query;
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            var data = _dbSet.Update(entity);
+            return data.Entity;
+        }
+
+        public IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+
+    }
+}
---
+++ ContactApp.API/Interfaces/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ContactApp.API.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity : class, IEntity
+    {
+        Task<TEntity> GetByIdAsync(object id);
+        TEntity GetById(object id);
+        IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
+        IQueryable<TEntity> JoinWhere(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity,object>>[] includes);
+        Task<TEntity> InsertAsyncReturn(TEntity entity);
+        Task InsertAsync(TEntity entity);
+        void Insert(TEntity entity);
+        TEntity InsertReturn(TEntity entity);
+        void Delete(TEntity entity);
+        void Delete(object id);
+        TEntity Update(TEntity entity);
+    }
+}
