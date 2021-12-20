44f84f5c8c3d578b558d6f590901a05d324d8be1
Hasan Erdal <hasaerda@hotmail.com>
2021-12-20 10:50:53 +0300

    Create Base Entity Model Class

---
+++ ContactApp.API/Data/Model/BaseModel.cs
@@ -0,0 +1,19 @@
+﻿using ContactApp.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactApp.API.Data.Model
+{
+    public abstract class BaseModel : IEntity
+    {
+        //public BaseModel()
+        //{
+        //    CreatedAt = UpdatedAt = DateTime.UtcNow;
+        //}
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
