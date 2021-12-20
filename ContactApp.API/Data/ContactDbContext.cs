107743fcf2d0c20501fbdf5df3b86a283de51f45
Hasan Erdal <hasaerda@hotmail.com>
2021-12-20 12:01:03 +0300

    Create ContactDbContext class

---
+++ ContactApp.API/Data/ContactDbContext.cs
@@ -0,0 +1,22 @@
+﻿using ContactApp.API.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactApp.API.Data
+{
+    public class ContactDbContext : DbContext
+    {
+        public ContactDbContext(DbContextOptions<ContactDbContext> options):base(options)
+        {
+        }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<ContactInfo> ContactInfos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
