3db0d7ad9d21b9fd5749eb58fce0ccf83460e81b
Hasan Erdal <hasaerda@hotmail.com>
2021-12-20 11:07:06 +0300

    Create Contact and ContactInfo Model Classes

---
+++ ContactApp.API/Data/Model/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactApp.API.Data.Model
+{
+    public class Contact : BaseModel
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Firm { get; set; }
+        public ICollection<ContactInfo> ContactInfos { get; set; }
+    }
+}
---
+++ ContactApp.API/Data/Model/ContactInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactApp.API.Data.Model
+{
+    public class ContactInfo
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// Information Type One Of "Telefon Numarası, E-Mail Adresi, Konum"
+        /// </summary>
+        public string Key { get; set; }
+        /// <summary>
+        /// Information Type Content
+        /// </summary>
+        public string Value { get; set; }
+        public string ContactId { get; set; }
+        public Contact Contact { get; set; }
+    }
+}
