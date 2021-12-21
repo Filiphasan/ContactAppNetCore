0062a089255664b34a820949393cacc2ed6a7f44
Hasan Erdal <hasaerda@hotmail.com>
2021-12-21 10:26:27 +0300

    Create ContactInfo Models

---
+++ ContactApp.API/Model/ContactGetModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactApp.API.Model
+{
+    public class ContactGetModel
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Firm { get; set; }
+        public IEnumerable<ContactInfoGetModel> ContactInfos { get; set; }
+    }
+}
---
+++ ContactApp.API/Model/ContactInfoAddModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactApp.API.Model
+{
+    public class ContactInfoAddModel
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public string ContactId { get; set; }
+    }
+}
