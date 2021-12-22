330473ac9ebc268b4f72c0542fe13da94ce6d262
Hasan Erdal <hasaerda@hotmail.com>
2021-12-22 20:32:37 +0300

    Create Contact Repository Test

---
+++ ContactApp.API.Test/Data/Repositories/ContactRepositoryTest.cs
@@ -0,0 +1,21 @@
+﻿using ContactApp.API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactApp.API.Test.Data.Repositories
+{
+    public class ContactRepositoryTest
+    {
+        //private readonly DbContext _context;
+        //public ContactRepositoryTest()
+        //{
+        //    IDbContextOptions dbContextOptions = 
+        //    _context = new ContactDbContext();
+        //}
+    }
+}
